99ffcd25d00b1b7c10215705ef6bcb1d915da44c
GBS3 <70548443+GBS3@users.noreply.github.com>
2021-02-20 14:40:25 -0600

    Update constants.py

--- myalbum_dl/constants.py
+++ myalbum_dl/constants.py
@@ -1,11 +1,4 @@
-HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-    'accept': 'application/json',
-    'accept-encoding': 'gzip, deflate, br',
-    'connection': 'keep-alive',
-    'x-requested-with': 'XMLHttpRequest',
-    'accept-language': 'en-us',
-}
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36'
 
 DOMAINS = [
     'https://thumbs-us-east-1.myalbum.io/',
@@ -20,6 +13,13 @@ SPINNERS = [
     '⠼',
 ]
 
+WIN_SPINNERS = [
+    '|',
+    '/',
+    '-',
+    '\\',
+]
+
 COLORS = [
     ('\033[38;5;9m', '\033[0m'),
     ('\033[38;5;202m', '\033[0m'),
@@ -29,12 +29,3 @@ COLORS = [
     ('\033[38;5;207m', '\033[0m'),
     ('\033[38;5;57m', '\033[0m'),
 ]
-
-WIN_SPINNERS = [
-    '|',
-    '/',
-    '-',
-    '\\',
-]
-
-WIN_COLORS = [('', ''), ]
